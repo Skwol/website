fcb14fd6e7b537255e41fa125f58a1d85543c366
ivan.rozhkov <rozhkov92@gmail.com>
2017-05-21 15:15:35 +0300

    fix dynamic dahboard

--- content_management.py
+++ content_management.py
@@ -1,2 +1,21 @@
 def Content():
-	return {'Python': [['Step 1', [('IDE: Sublime/PyCharm/etc', '/step-1-ide/'), ('PEP guide', '/step-1-pep/'), ('Package installers', '/step-1-package-managers/')]], ['Step 2', [('Iterators and comprehensions', '/step-2-iter-and-compr/'), ('Lambda-functions', '/step-2-lambda/'), ('Generators', '/step-2-generators/') ('Text parsing, working with files and unicode', '/step-2-parsing-files-unicode/'), ('Error handling', '/step-2-error-handling/'),('Modules', '/step-2-modules/'),('IDE: Chrome WEbDev Tools, Fiddler etc', '/step-2-web-ide/')]]]}
+    return {
+        'Python': [
+            ['Step 1', [
+                ('IDE: Sublime/PyCharm/etc', '/step-1-ide/'),
+                ('PEP guide', '/step-1-pep/'),
+                ('Package installers', '/step-1-package-managers/')
+            ]
+             ],
+            ['Step 2', [
+                ('Iterators and comprehensions', '/step-2-iter-and-compr/'),
+                ('Lambda-functions', '/step-2-lambda/'),
+                ('Generators', '/step-2-generators/'),
+                ('Text parsing, working with files and unicode', '/step-2-parsing-files-unicode/'),
+                ('Error handling', '/step-2-error-handling/'),
+                ('Modules', '/step-2-modules/'),
+                ('IDE: Chrome WEbDev Tools, Fiddler etc', '/step-2-web-ide/')
+            ]
+             ]
+        ]
+    }
